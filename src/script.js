20b19f91394324e61cced00463fc33b12ac219ce
chezanh <108699996+chezanh@users.noreply.github.com>
2025-03-04 10:45:52 +0100

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,6 @@
+console.log("JavaScript is connected!");
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("h1").classList.add("show");
+  document.querySelector("h2").classList.add("show");
+});
